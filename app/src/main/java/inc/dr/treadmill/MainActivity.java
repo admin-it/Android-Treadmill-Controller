4329812f8571deb2508a7ceeba4851c09f2fa56b
pdllvlle <pdllvlle@memphis.edu>
2016-02-29 14:18:41 -0600

    Test for Repository sync

--- app/src/main/java/inc/dr/treadmill/MainActivity.java
+++ app/src/main/java/inc/dr/treadmill/MainActivity.java
@@ -3,6 +3,8 @@ package inc.dr.treadmill;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+//Created by Enyil Valle (pdllvlle)
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
